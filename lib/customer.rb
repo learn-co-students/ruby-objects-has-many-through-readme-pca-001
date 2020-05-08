a384ec6fb8162f48b5b2c2c4b854ef673e3a247b
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-08 11:43:32 -0400

    lab complete

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+  attr_reader :name, :age, :meals, :waiters
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @meals = []
+    @waiters = []
+    @@all << self
+  end
+  def new_meal(new_waiter, total, tip)
+    meal = Meal.new(new_waiter, self, total, tip)
+    @meals << meal
+    new_waiter.meals << meal
+    @waiters << new_waiter
+    meal
+  end
+  class << self
+    def all
+      @@all
+    end
+  end
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
-
+  attr_reader :waiter, :customer, :total, :tip
+  @@all = []
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+  class << self
+    def all
+      @@all
+    end
+  end
 end
